9109e65c90657e5aa15ece60afc062be4530d740
phuriphat-tnk <67319010032@tnk.ac.th>
2024-11-12 16:36:01 +0700

    Update Personal Profile.py

--- python_oop_week5/Personal Profile.py
+++ python_oop_week5/Personal Profile.py
@@ -1,7 +1,7 @@
 a = ()
-me = { "name": a , "nickname": a , "stdid": a , "hobby": a ,"color" : a}
 num = int(input('ใส่จำนวนคนที่จะป้อน : '))
 for i in range(num):
+    me = {}
     print("กรุณากรอกข้อมูลคนที่ ",(i+1))
     a = input("กรุณากรอกชื่อ : ")
     me["name"] = a
